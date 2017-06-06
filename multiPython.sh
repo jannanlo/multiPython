40d3ef766bfad398feedb81033cc4d80a7be26cf
Jenner Luo <jannanlo@163.com>
2017-06-06 11:51:58 +0800

    add download_python method

--- multiPython.sh
+++ multiPython.sh
@@ -1,4 +1,19 @@
+#!/bin/bash
+#Time:2017-06-06
+#Author:Jenner.Luo
 
+PWD=$(pwd)
 
+download_python(){
+    local py_url=$1
+    if [[ ! ${py_url} ]]; then
+        py_url=https://www.python.org/ftp/python/2.7.13/Python-2.7.13.tgz
+    fi
+    local pkg_dir=${PWD}/softwares
+    if [[ ! - ${pkg_dir} ]]; then
+        mkdir ${pkg_dir}
+    fi
+    wget -P ${pkg_dir} ${py_url}
 
-wget https://www.python.org/ftp/python/2.7.13/Python-2.7.13.tgz
+    echo ${PWD}/${py_url##*/}   # Return package path
+}
