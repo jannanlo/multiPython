5bba4e799759e3c85ff3a86ec0c56f8f43b108f8
Jenner Luo <jannanlo@163.com>
2017-06-06 13:37:13 +0800

    add make_install method

--- multiPython.sh
+++ multiPython.sh
@@ -6,14 +6,24 @@ PWD=$(pwd)
 
 download_python(){
     local py_url=$1
-    if [[ ! ${py_url} ]]; then
+    if [[ -z ${py_url} ]]; then
         py_url=https://www.python.org/ftp/python/2.7.13/Python-2.7.13.tgz
     fi
     local pkg_dir=${PWD}/softwares
-    if [[ ! - ${pkg_dir} ]]; then
+    if [[ ! -d ${pkg_dir} ]]; then
         mkdir ${pkg_dir}
     fi
     wget -P ${pkg_dir} ${py_url}
 
     echo ${PWD}/${py_url##*/}   # Return package path
+}
+
+make_install(){
+    local pkg_path=$1
+    if [[ ! -z ${pkg_path} ]]; then
+        local pkg_dir=${pkg_path%/*}
+        local pkg_name=${pkg_path##/*}
+        cd ${pkg_dir}
+        tar -zxvf ${pkg_name}
+    fi
 }
