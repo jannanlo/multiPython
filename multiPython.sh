75f430c9c11673e05a79b49434ddbe60854a40f0
Jenner Luo <jannanlo@163.com>
2017-07-10 14:58:21 +0800

    fix main

--- multiPython.sh
+++ multiPython.sh
@@ -57,6 +57,10 @@ main(){
 
 
 #make_install /data/Python-2.7.13.tgz
+#download_pkg "https://pypi.python.org/packages/11/b6/abcb525026a4be042b486df43905d6893fb04f05aac21c32c638e939e447/pip-9.0.1.tar.gz#md5=35f01da33009719497f01a4ba69d63c9 --no-check-certificate"
+#download_pkg "https://pypi.python.org/packages/a9/23/720c7558ba6ad3e0f5ad01e0d6ea2288b486da32f053c73e259f7c392042/setuptools-36.0.1.zip#md5=430eb106788183eefe9f444a300007f0 --no-check-certificate"
+#install_tar_pkg /usr/local/bin/python2.7 /data/softwares/setuptools-36.0.1.zip
+#install_tar_pkg /usr/local/bin/python2.7 /data/softwares/pip-9.0.1.tar.gz
 #
 #
 ##alias python=/usr/local/bin/python2.7
